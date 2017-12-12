d8e79daf7ff1a3baea7602d95d32a23d3970a9ae
dblink-eu <grzegorz.zasadzinski@dblink.eu>
2017-12-12 19:58:40 +0100

    Second Commit

--- src/main/java/com/kodilla/testing/TestingMain.java
+++ src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
         public static void main(String[] args) {
-            System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+            //System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+            SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+            String result = simpleUser.getUsername();
+
+            if (result.equals("theForumUser")){
+                System.out.println("Test OK");
+            } else {
+                System.out.println("Error!");
+            }
+
+            //Test Calculator
+            int a=5;
+            int b=3;
+            Calculator calc = new Calculator(a,b);
+            int addResult = calc.add();
+            if (addResult==(a+b)) {
+                System.out.println("Test OK");
+            } else {
+                System.out.println("Error");
+            }
+
+            int subResult = calc.subtract();
+            if (subResult==(a-b)) {
+                System.out.println("Test OK");
+            } else {
+                System.out.println("Error");
+            }
+
         }
 }
--- src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,4 +1,21 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a=a;
+        this.b=b;
+    }
+
+    public int add() {
+        int resultAdd = a+b;
+        return resultAdd;
+    }
+
+    public int subtract() {
+        int resultSubtract = a-b;
+        return resultSubtract;
+    }
 }
