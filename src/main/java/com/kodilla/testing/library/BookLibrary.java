cbd7224ac29d7dfa852372072d9f7b127509914c
dblink-eu <grzegorz.zasadzinski@dblink.eu>
2018-01-08 22:56:33 +0100

    6.7

---
+++ src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -0,0 +1,35 @@
+package com.kodilla.testing.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookLibrary {
+    private LibraryDatabase libraryDatabase;
+
+    public BookLibrary(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+    }
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        List<Book> bookList = new ArrayList<Book>();
+        Book book = new Book("The book title", "The book author", 2000);
+        bookList.add(book);
+
+        // temporary returning list of one book
+        return bookList;
+    }
+
+    public List<Book> listBooksInHandsOfUser(LibraryUser libraryUser) {
+        List<Book> usersBooksList = libraryDatabase.listBooksInHandsOf(libraryUser);
+        if(usersBooksList.size()==0){
+            System.out.println("User dont' have any books");
+        }
+        else{
+            System.out.println("User: "+ libraryUser.getFirstname() + " "  + libraryUser.getLastname() + " has been borrowed:");
+            for (Book usersBooks:usersBooksList) {
+                System.out.println(usersBooks);
+            }
+        }
+        return usersBooksList;
+    }
+}
